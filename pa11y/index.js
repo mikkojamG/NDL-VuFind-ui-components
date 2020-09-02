436b42b34a4c5e58991c7887b1161ec9fbeb9423
mikkojamG <mikko.jamia@gofore.com>
2020-09-02 13:40:06 +0300

    Log progress

--- pa11y/index.js
+++ pa11y/index.js
@@ -1,51 +1,90 @@
-require('events').EventEmitter.defaultMaxListeners = 100
-
 const path = require('path');
 const pa11y = require('pa11y');
 const fs = require('fs');
+const chalk = require('chalk');
+const cliProgress = require('cli-progress');
 
 const patternlab = require('../patternlab-config.json');
 const config = require('./settings');
 
-const getMarkupFiles = (dir) => {
-  return fs.readdirSync(dir).reduce((files, file) => {
-    const name = path.join(dir, file);
+const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
-    const isDirectory = fs.statSync(name).isDirectory();
+const getPatterns = (directory) => {
+  return fs.readdirSync(directory)
+    .reduce((files, file) => {
+      const name = path.join(directory, file);
 
-    return isDirectory ? [...files, ...getMarkupFiles(name)] : [...files, name];
-  }, []).filter((file) => {
-    const name = path.join(dir, file);
+      const isDirectory = fs.statSync(name).isDirectory();
 
-    const isMarkupOnly = name.indexOf('.markup-only') > 0;
-    const isPartial = name.indexOf('-_') > 0;
+      return isDirectory ? [...files, ...getPatterns(name)] : [...files, name];
+    }, [])
+    .filter((file) => {
+      const name = path.join(directory, file);
 
-    return isMarkupOnly && !isPartial;
-  });
-}
+      const isMarkupOnly = name.indexOf('.markup-only') > 0;
+      const isHelperPattern = name.indexOf('-_') > 0;
 
-const getIssues = async files => Promise.all(files.map(async (file) => {
-  const results = await pa11y(file, config);
+      return isMarkupOnly && !isHelperPattern;
+    });
+};
 
-  const issues = results.issues;
-
-  if (issues.length) {
-    return {
-      pattern: path.basename(file),
-      issues: issues
-    }
+const asyncForEach = async (array, callback) => {
+  for (const item of array) {
+    await callback(item);
   }
-}));
+};
+
+const getPatternIssuesPromise = (files) => {
+  return Promise.all(files.map(async (file) => {
+    const results = await pa11y(file, config);
+
+    progress.increment();
+
+    const issues = results.issues;
+
+    if (issues.length) {
+      return {
+        pattern: path.basename(file),
+        issues: issues
+      }
+    }
+  }));
+};
+
+const getPatternIssues = async (files) => {
+  progress.start(files.length, 0);
+
+  const issues = [];
+
+  await asyncForEach(files, async (file) => {
+    const results = await pa11y(file, config);
+
+    const resultsIssues = results.issues;
+
+    issues.push({ pattern: path.basename(file), issues: resultsIssues })
+
+    progress.increment();
+  });
+
+  progress.stop();
+
+  return issues;
+}
 
 const trackIssues = async () => {
   try {
-    const files = getMarkupFiles(path.resolve(patternlab.paths.public.patterns));
+    console.log(chalk.yellow('Starting accessiblity testing for patterns. Please wait..'))
 
-    const issues = await getIssues(files);
+    const files = getPatterns(path.resolve(patternlab.paths.public.patterns));
+
+    const issues = await getPatternIssues(files);
 
     console.dir(issues, { depth: null });
+    console.log(chalk.green('Accessibility testing finished successfully!'));
   } catch (error) {
     console.log(error);
+
+    console.log(chalk.red('Something went wrong! Error: ' + error));
   }
 };
 
