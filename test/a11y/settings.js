cbd77cfa3681afd87821e6c39f349cf21cfd1e1c
mikkojamG <mikko.jamia@gofore.com>
2020-09-02 14:33:45 +0300

    Cleanup

--- test/a11y/settings.js
+++ test/a11y/settings.js
@@ -4,10 +4,6 @@ module.exports = {
   runners: ['htmlcs', 'axe'],
   ignore: [
     'page-has-heading-one',
-    'html-has-lang',
-    'document-title',
-    'WCAG2AA.Principle3.Guideline3_1.3_1_1.H57.2',
-    'WCAG2AA.Principle2.Guideline2_4.2_4_2.H25.1.NoTitleEl',
     'bypass',
     'landmark-one-main',
     'region'
