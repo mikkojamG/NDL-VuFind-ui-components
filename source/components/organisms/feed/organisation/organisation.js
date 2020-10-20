5b1847cbbc61c3e89d2885a81878835f5114b69c
mikkojamG <mikko.jamia@gofore.com>
2020-10-20 12:06:01 +0300

    Error handling

--- source/components/organisms/feed/organisation/organisation.js
+++ source/components/organisms/feed/organisation/organisation.js
@@ -1,21 +1,20 @@
 /* global VuFind, finna */
 finna.organisationFeed = (function organisationFeed() {
-  var $holder, $spinner;
-
+  var $holder, $grid, $spinner, $error;
   var service;
 
   var getFeedUrl = function getFeedUrl() {
     var deferred = $.Deferred();
 
-    var parent = $holder.data('parent');
-    var id = $holder.data('organisation-id');
+    var parent = $grid.data('parent');
+    var id = $grid.data('organisation-id');
 
     service.getOrganisations('page', parent, {}, {},
       function onOrganisationsLoaded() {
         service.getSchedules('page', parent, id, null, null, true, true,
           function onSchedulesLoaded(res) {
             var rss = res.rss.filter(function findFeedRss(item) {
-              return item.id === $holder.data('feed-id');
+              return item.id === $grid.data('feed-id');
             })[0];
 
             deferred.resolve(rss.url);
@@ -35,35 +34,43 @@ finna.organisationFeed = (function organisationFeed() {
       headers: {
         'Content-Type': 'application/json'
       }
-    })
-      .done(function onRequestDone(res) {
-        var response = JSON.parse(res);
+    }).done(function onRequestDone(res) {
+      var response = JSON.parse(res);
 
-        if (response.data) {
-          $spinner.addClass('hide');
+      if (response.data) {
+        $spinner.addClass('hide');
 
-          $holder.html(response.data.html);
+        $grid.html(response.data.html);
 
-          var settings = response.data.settings;
+        var settings = response.data.settings;
 
-          if (!settings.height) {
-            settings.height = 300;
-          }
-
-          if (settings.modal) {
-            $holder.find('a').on('click', function onClickModal() {
-              $('#modal').addClass('feed-content');
-            });
-
-            VuFind.lightbox.bind($holder);
-          }
+        if (!settings.height) {
+          settings.height = 300;
         }
-      })
-      .fail(function onRequestFail() {
-      })
+
+        if (settings.modal) {
+          $grid.find('a').on('click', function onClickModal() {
+            $('#modal').addClass('feed-content');
+          });
+
+          VuFind.lightbox.bind($grid);
+        }
+      }
+    }).fail(function onRequestFail(err) {
+      $spinner.addClass('hide');
+
+      if (err.responseJSON) {
+        $error.html(err.responseJSON.data);
+      }
+
+      $error.removeClass('hide');
+    });
   };
 
   var loadFeed = function loadFeed(params) {
+    $spinner.removeClass('hide');
+    $error.addClass('hide');
+
     params['touch-device'] = (finna.layout.isTouchDevice() ? 1 : 0);
     params.method = 'getOrganisationPageFeed';
 
@@ -82,7 +89,9 @@ finna.organisationFeed = (function organisationFeed() {
     loadFeed: loadFeed,
     init: function init(holder, _service) {
       $holder = holder;
+      $grid = $holder.find('.js-feed-grid');
       $spinner = $holder.find('.js-loader');
+      $error = $holder.find('.js-feed-error');
 
       service = _service;
     }
