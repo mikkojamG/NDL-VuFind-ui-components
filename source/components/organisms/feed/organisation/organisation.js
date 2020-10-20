6c52d1e658a06eb2a6e0b4e4cab60166a91adfc2
mikkojamG <mikko.jamia@gofore.com>
2020-10-20 12:55:26 +0300

    VuFind testing fixes

--- source/components/organisms/feed/organisation/organisation.js
+++ source/components/organisms/feed/organisation/organisation.js
@@ -60,6 +60,22 @@ finna.organisationFeed = (function organisationFeed() {
           VuFind.lightbox.bind($grid);
         }
       }
+
+      if ($grid.find('grid-item.truncate').length) {
+        $grid.find('.show-more-feeds').removeClass('hidden');
+      }
+
+      $grid.find('.show-more-feeds').on('click', function showMoreFeeds() {
+        $grid.find('.grid-item.truncate').removeClass('hidden');
+        $grid.find('.show-less-feeds').removeClass('hidden');
+        $(this).addClass('hidden');
+      });
+
+      $grid.find('.show-less-feeds').click(function showLessButton() {
+        $grid.find('.grid-item.truncate').addClass('hidden');
+        $grid.find('.show-more-feeds').removeClass('hidden');
+        $(this).addClass('hidden');
+      });
     }).fail(function onRequestFail(err) {
       $spinner.addClass('hide');
 
