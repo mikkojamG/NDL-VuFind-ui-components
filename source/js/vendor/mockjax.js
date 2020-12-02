31b5010e4f5c68329da8320fadf2745cf280f754
Mikko Jämiä <66477579+mikkojamG@users.noreply.github.com>
2020-12-02 16:39:36 +0200

    feat/organism-feed-tabs (#41)
    * Start nav tabs template
    
    * Helper template and example JS
    
    * Mobile tabs
    
    * Use existing dropdown component
    
    * Move main tabs to organisms
    
    * Tabs helper
    
    * Mobile dropdown menu width
    
    * Tab molecule documentation
    
    * Update dropdown documentation
    
    * Main tabs navigation
    
    * Submit is required
    
    * Tabs with tooltip
    
    * Tooltip fixes
    
    * Tabs tooltip organism
    
    * Example Javascript fixes
    
    * Undefined variable fix
    
    * Move tab organisms under same folder
    
    * Move dropdowns and tabs under navigation folder
    
    * Documentation fixes
    
    * Dropdown fallback label
    
    * Accessibility fixes
    
    * Remove unnecessary suffix
    
    * Start heading tabs template
    
    * Make sure background-color stays consistent in inactive tabs
    
    * Mock feed
    
    * Wildcard mock
    
    * Use minified JS
    
    * Remove extra JS
    
    * Rename organism
    
    * Documentation
    
    * Edit documentation
    
    * Move JS to patternlab folder
    
    * Remove unnecessary JS
    
    * Remove extra PL helper
    
    * Shorten ternary operators
    
    * Move components to subdirectories
    
    * Update documentation
    
    * Drop prefix from component names
    
    * Drop prefixes from components
    
    * Fix filename typo
    
    * Move feed tabs to PL templates only
    
    * Fix weird tooltip focus bug
    
    * Set focus fixes to finna-main-tabs
    
    * For presentational purposes don't activate tab when clicking tooltip
    
    * JS bugfix
    
    * Merge master, drop number prefixes
    
    * PR fixes
    
    * Remove mustache templates
    
    * Pattern state, use inlineScript

--- source/js/vendor/mockjax.js
+++ source/js/vendor/mockjax.js
@@ -8,47 +8,61 @@ var mockTags = [{
 }
 ];
 
-$.mockjax([{
-  url: '/finna/tags',
-  type: 'GET',
-  responseTime: 2000,
-  response: function() {
-    this.responseText = {
-      'tags': mockTags
-    };
-  }
-},
-{
-  url: '/finna/tags',
-  type: 'POST',
-  responseTime: 1000,
-  response: function(settings) {
-    mockTags.push({
-      id: Math.floor(Math.random() * 100 + 1).toString(),
-      value: JSON.parse(settings.data).tag
-    });
+$.mockjax([
+  {
+    url: '/finna/tags',
+    type: 'GET',
+    responseTime: 2000,
+    response: function () {
+      this.responseText = {
+        'tags': mockTags
+      };
+    }
+  },
+  {
+    url: '/finna/tags',
+    type: 'POST',
+    responseTime: 1000,
+    response: function (settings) {
+      mockTags.push({
+        id: Math.floor(Math.random() * 100 + 1).toString(),
+        value: JSON.parse(settings.data).tag
+      });
 
-    this.responseText = {
-      'status': 'OK'
-    };
-  }
-},
-{
-  url: '/finna/tags',
-  type: 'DELETE',
-  responseTime: 1000,
-  response: function(settings) {
-    var deleteTagId = JSON.parse(settings.data).id;
+      this.responseText = {
+        'status': 'OK'
+      };
+    }
+  },
+  {
+    url: '/finna/tags',
+    type: 'DELETE',
+    responseTime: 1000,
+    response: function (settings) {
+      var deleteTagId = JSON.parse(settings.data).id;
 
-    var newTags = mockTags.filter(function(tag) {
-      return tag.id !== deleteTagId;
-    });
+      var newTags = mockTags.filter(function (tag) {
+        return tag.id !== deleteTagId;
+      });
 
-    mockTags = newTags.slice(0);
+      mockTags = newTags.slice(0);
 
-    this.responseText = {
-      'status': 'OK'
-    };
-  }
-}
+      this.responseText = {
+        'status': 'OK'
+      };
+    }
+  },
+  {
+    url: VuFind.path + '/AJAX/JSON?method=getFeed&id=*&touch-device=*',
+    type: 'GET',
+    responseTime: 1000,
+    response: function () {
+      this.responseText = {
+        data: {
+          html: "<div><p>Feed Content</p><div>",
+          settings: { type: "grid", modal: false }
+        }
+      }
+    }
+  },
 ]);
