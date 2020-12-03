c2bb544d1eb788bdc9658c5ab6073fcb3910bfab
Mikko Jämiä <66477579+mikkojamG@users.noreply.github.com>
2020-12-03 11:24:26 +0200

    feat/organism-schedule (#49)
    * Merge master
    
    Fix paths
    
    feat/js-component-imports (#43)
    
    * JS imports task
    
    * Write imports into components.config.php
    
    * Cleanup
    
    refactor/less-imports-changes (#45)
    
    * Import without gulp-inject plugin
    
    * Change task description
    
    * Cleanup
    
    fix/gulp-fix-symlinks (#46)
    
    * Fix symlinks paths
    
    * Fix paths
    
    refactor/pseudo-patterns (#44)
    
    * Remove prefixes, root level
    
    * Drop atoms prefixes
    
    * Refactor atoms structure
    
    * Fix template error
    
    * Drop molecule prefixes
    
    * Drop organisms prefixes
    
    * Refactor sort
    
    * Markdown fixes
    
    * Edit markdown
    
    * Add missing documentation
    
    * Edit markdown
    
    * Edit markdown
    
    * Edit markdown
    
    * Buttons, used pseudo
    
    * Colors, use pseudo
    
    * Icons use pseudo
    
    * Tabs molecule, use pseudo
    
    * Tabs organism, use pseudo
    
    * Tooltip use pseudo
    
    * Pattern titles
    
    * Ordered table component
    
    * Documentation fixes
    
    * Update PL config
    
    * Revert unrelated config change
    
    * Rename function
    
    * Renaming patterns
    
    * Update documentation
    
    * Loader molecule
    
    * Timetable templates
    
    * Not available block
    
    * Component draft
    
    * Start refactoring JS
    
    * Cleanup
    
    * Remote mustache templates
    
    * Add widget details
    
    * JS changes
    
    * JS selector classes
    
    * Add styles
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Mock data working
    
    * Append templates
    
    * Append templates
    
    * Fix time-container
    
    * Fix handling multiple time periods
    
    * Cleanup
    
    * Cleanup
    
    * Template and json cleanup
    
    * JS cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Pager component
    
    * Loader template fix
    
    * Keywords loader fix
    
    * Button components
    
    * Use button component
    
    * Mock changing weeks
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Accessibility fix
    
    * Accessibility fix
    
    * Cleanup
    
    * Unnecessary component
    
    * Cleanup
    
    * Cleanup
    
    * Add documentation
    
    * JS bug fix
    
    * Template fix
    
    * Template fixes
    
    * Create demo template
    
    * Use all properties for demo
    
    * Revert unrelevant changes
    
    * Documentation fix
    
    * Accessibility fix
    
    * VuFind testing fixes
    
    * Spinner visibility changes
    
    * Spinner visibility changes
    
    * Remove page breaks
    
    * Cleanup
    
    * Cleanup
    
    * Revert unrelated changes
    
    * JS Cleanup
    
    * Fix typo
    
    * Organisations list bug fixes
    
    * Make id property optional
    
    * Fix spinner on load
    
    * Pager styles
    
    * Pager documentation
    
    * Documentation fix
    
    * JS cleanup
    
    * Documentation fix
    
    * Cleanup
    
    * Loader to atoms
    
    * Remove demo, pattern state, use inlineScript
    
    * Add patterns states
    
    * Testing fixes
    
    * Documentation fixes
    
    * Always fetch latest schedules
    
    * Documentation fixes, required properties
    
    * Listen to mapWidget changes
    
    * Use finna alert
    
    * Documentation
    
    * Better naming and documentation
    
    * Documentation fix
    
    * Schedule element into component
    
    * Accessibility fixes
    
    * Widget accessibility fixes
    
    * Unbind navi listener before binding again
    
    * Use polite status alert
    
    * Remove testing
    
    Co-authored-by: Aleksi Peebles <aleksi.peebles@helsinki.fi>

---
+++ source/components/atoms/messaging/loader/loader.md
@@ -0,0 +1,13 @@
+---
+state: inprogress
+---
+
+#### Description
+
+Loader component, used for indicating ongoing processes for the user.
+
+#### Properties
+
+| Prop         | Type     | Value (example) | Required |
+| ------------ | -------- | --------------- | -------- |
+| wrapperClass | `String` | -               | No       |
--- source/js/vendor/mockjax.js
+++ source/js/vendor/mockjax.js
@@ -8,6 +8,33 @@ var mockTags = [{
 }
 ];
 
+var mockOpeningTimes = [
+  { opens: "9", closes: "10", selfservice: true },
+  { opens: "10", closes: "17", selfservice: false },
+  { opens: "17", closes: "18", selfservice: true }
+];
+
+var getWeekForDate = function (date) {
+  var weekModel = ['Ma', 'Ti', 'Ke', 'To', 'Pe', 'La', 'Su'];
+
+  return weekModel.map(function (_, index) {
+    var first = date.getDate() - date.getDay() + (index + 1);
+    var day = new Date(date.setDate(first)).toISOString().slice(0, 10);
+
+    var dayArray = day.split('-').reverse();
+    var yearRemoved = dayArray.slice(0, -1);
+    var dateString = yearRemoved.join('.')
+
+    return dateString
+  });
+};
+
+var getWeekNumber = function (date) {
+  var oneJan = new Date(date.getFullYear(), 0, 1);
+
+  return Math.ceil((((date.getTime() - oneJan.getTime()) / 86400000) + oneJan.getDay() + 1) / 7);
+};
+
 $.mockjax([
   {
     url: '/finna/tags',
@@ -71,6 +98,176 @@ $.mockjax([
     responseTime: 1000,
     response: function () {
       this.responseText = "<div><p>Tab Record</p><div>";
+    }
+  },
+  {
+    url: VuFind.path + '/AJAX/JSON',
+    data: { method: 'getOrganisationInfo', params: { action: 'details' } },
+    type: 'GET',
+    responseTime: 1000,
+    response: function (settings) {
+      var params = settings.data.params;
+
+      if (params.dir) {
+        if (params.dir > 0) {
+          var dateWeekFromNow = new Date;
+          dateWeekFromNow.setDate(dateWeekFromNow.getDate() + 7);
+
+          var week = getWeekForDate(dateWeekFromNow);
+          var weekNumber = getWeekNumber(new Date) + 1;
+        } else {
+          var week = getWeekForDate(new Date);
+          var weekNumber = getWeekNumber(new Date);
+        }
+        this.responseText = {
+          data: {
+            openTimes: {
+              schedules: [
+                {
+                  date: week[0],
+                  times: mockOpeningTimes,
+                  day: "Ma",
+                  today: true
+                },
+                {
+                  date: week[1],
+                  times: mockOpeningTimes,
+                  day: "Ti"
+                },
+                {
+                  date: week[2],
+                  times: mockOpeningTimes,
+                  day: "Ke"
+                },
+                {
+                  date: week[3],
+                  times: mockOpeningTimes,
+                  day: "To"
+                },
+                {
+                  date: week[4],
+                  times: mockOpeningTimes,
+                  day: "Pe"
+                },
+                { date: week[5], times: [], day: "La", closed: true },
+                { date: week[6], times: [], day: "Su", closed: true }
+              ],
+              currentWeek: weekNumber == getWeekNumber(new Date),
+            },
+            id: "86154",
+            periodStart: "2020-09-28",
+            weekNum: weekNumber
+          }
+        }
+      } else {
+        this.responseText = {
+          data: {
+            phone: "<ul>\n  <li><p><i class=\"fa fa-phone-square\"></i><a href=\"tel:02&#x20;9412&#x20;3196\">02 9412 3196</a> / Asiakaspalvelu</p></li>\n</ul>\n",
+            emails: "<ul>\n  <li><p><i class=\"fa fa-envelope\"></i><a href=\"mailto:kk-palvelu&#x40;helsinki.fi\">kk-palvelu@helsinki.fi</a></p></li>\n</ul>\n",
+            pictures: [
+              {
+                url: "https://kirkanta.kirjastot.fi/files/photos/medium/kanki-k-590867ab71c76.jpg",
+                size: 643166,
+                resolution: "1543x1000"
+              }
+            ],
+            links: [
+              { name: "Twitter", url: "https://twitter.com/NatLibFi" },
+              {
+                name: "Instagram",
+                url: "https://www.instagram.com/kansalliskirjasto/"
+              },
+              {
+                name: "Facebook",
+                url: "https://www.facebook.com/Kansalliskirjasto/"
+              },
+              {
+                name: "Kirjaston kotisivut",
+                url: "http://www.kansalliskirjasto.fi/"
+              }
+            ],
+            services: [
+              'wifi',
+              'print'
+            ],
+            id: "86154",
+            periodStart: "2020-09-14",
+            weekNum: weekNumber
+          }
+        }
+      }
+    }
+  },
+  {
+    url: VuFind.path + '/AJAX/JSON',
+    data: { method: 'getOrganisationInfo', params: { action: 'consortium' } },
+    type: 'GET',
+    responseTime: 1000,
+    response: function () {
+      var week = getWeekForDate(new Date);
+      var weekNumber = getWeekNumber(new Date);
+
+      this.responseText = {
+        data: {
+          consortium: {
+            finna: {
+              service_point: 86154
+            },
+          },
+          list: [
+            {
+              id: 86154,
+              name: "Kansalliskirjasto",
+              mobile: 0,
+              email: "kk-palvelu@helsinki.fi",
+              address: {
+                street: "Unioninkatu 36",
+                zipcode: "00170",
+                city: "Helsinki",
+                coordinates: { "lat": 60.17037, "lon": 24.95034 }
+              },
+              routeUrl: "https://opas.matka.fi/?to=Unioninkatu%2036,%20Helsinki",
+              mapUrl: "http://maps.google.com/?q=Unioninkatu%2036%20Helsinki",
+              openTimes: {
+                schedules: [
+                  {
+                    date: week[0],
+                    times: mockOpeningTimes,
+                    day: "Ma",
+                    today: true
+                  },
+                  {
+                    date: week[1],
+                    times: mockOpeningTimes,
+                    day: "Ti"
+                  },
+                  {
+                    date: week[2],
+                    times: mockOpeningTimes,
+                    day: "Ke"
+                  },
+                  {
+                    date: week[3],
+                    times: mockOpeningTimes,
+                    day: "To",
+                  },
+                  {
+                    date: week[4],
+                    times: mockOpeningTimes,
+                    day: "Pe"
+                  },
+                  { date: week[5], times: [], day: "La", "closed": true },
+                  { date: week[6], times: [], day: "Su", "closed": true }
+                ],
+                currentWeek: true,
+              },
+              openNow: true
+            }
+          ],
+          id: "86154",
+          weekNum: weekNumber
+        }
+      }
     }
   }
 ]);
